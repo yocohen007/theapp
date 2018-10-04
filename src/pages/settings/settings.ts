5f3fa09753d092473d5c16e486b2ff3a5bd112a4
yocohen007 <yocohen@gmail.com>
2018-10-04 22:20:23 +0300

    translate alert

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, NavParams, AlertController } from "ionic-angular";
 import { Storage } from "@ionic/storage";
 import { TranslateService } from "@ngx-translate/core";
 
@@ -16,14 +16,15 @@ import { TranslateService } from "@ngx-translate/core";
   templateUrl: "settings.html"
 })
 export class SettingsPage {
-  lang: string = "heb";
+  lang: string;
   private readonly LANGUAGE_STORAGE_KEY: string = "digitize.theapp.lang";
 
   constructor(
     public translate: TranslateService,
     public navCtrl: NavController,
     public navParams: NavParams,
-    private storage: Storage
+    private storage: Storage,
+    public alertCtrl: AlertController
   ) {
     this.storage.get(this.LANGUAGE_STORAGE_KEY).then(val => {
       if (val != null) {
@@ -38,8 +39,15 @@ export class SettingsPage {
   }
 
   public onLangChange(event): void {
-    console.log("lang="+this.lang);
+    console.log("lang=" + this.lang);
+    let alert = this.alertCtrl.create({
+      subTitle: 'Some changes may take affect only after app restart.',
+      buttons: ['Ok']
+    });
+
+    alert.present();
     this.storage.set(this.LANGUAGE_STORAGE_KEY, this.lang);
     this.translate.use(this.lang);
+
   }
 }
